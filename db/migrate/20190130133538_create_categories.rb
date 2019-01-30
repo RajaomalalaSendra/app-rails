129920c86980adca9454ca850933837113c49f61
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-30 16:45:40 +0300

    adding categories

--- db/migrate/20190130133538_create_categories.rb
+++ db/migrate/20190130133538_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :name
-      t.belong_to :article, index: true
+      t.belongs_to :article, index: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_121807) do
+ActiveRecord::Schema.define(version: 2019_01_30_133538) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2019_01_30_121807) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_categories_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
